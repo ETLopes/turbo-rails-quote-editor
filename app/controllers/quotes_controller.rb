7e8c0b1ab9d4ea7441c3ed4aa8c97fc339757bff
Eduardo Lopes <eduardotorreslopes@gmail.com>
2022-03-23 23:43:07 -0300

    feat: create quote controller

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,27 @@
+class QuotesController < ApplicationController
+    def index 
+        @quotes = Quote.all
+    end
+
+    def show
+    end
+
+    def new
+        @quote = Quote.new
+    end
+
+    def create
+        @quote = Quote.new(quote_params)
+
+        if @quote.save
+            redirect_to quotes_path, notice: "Quote was successfully created."
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+end
